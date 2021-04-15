5b5f3e862c6d3fac133d9cea7fc5e39d7b067c70
DonaldReac <40219825+DonaldReac@users.noreply.github.com>
2021-04-15 18:44:46 -0500

    Base de datos

---
+++ database/migrations/2021_04_14_201324_create_platillos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatillosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('platillos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->double('precio');
+            $table->integer('categoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('platillos');
+    }
+}
